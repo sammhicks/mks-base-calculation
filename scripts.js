0fd174da4d843e1d6118480e7a4da6ab006bdfab
Samuel Hicks <SamMHicks@hotmail.co.uk>
2016-11-30 19:25:01 +0000

    Wrote config file parser

--- scripts.js
+++ scripts.js
@@ -8,8 +8,16 @@ app.controller('mks-calculation-controller', function($scope, $http) {
 	{
 		return new Promise(function(resolve, reject){
 			$http.get(config_location).catch(reject).then((response) => {
-				var config = {};
-				resolve(config);
+				var parse_result = config_parser.config.parse(response.data);
+				
+				if (parse_result.status)
+				{
+					resolve(parse_result.value);
+				}
+				else
+				{
+					reject(parse_result);
+				}
 			});
 		});
 	}
@@ -19,7 +27,7 @@ app.controller('mks-calculation-controller', function($scope, $http) {
 		$scope.ksp_version = response.data.KSP_VERSION;
 	}).then(() => {
 		load_config("https://raw.githubusercontent.com/BobPalmer/MKS/master/FOR_RELEASE/GameData/UmbraSpaceIndustries/MKS/Parts/Duna_Agriculture.cfg").then((response) => {
-			console.log(response.data);
+			console.log(response);
 		});
 	});
 });
