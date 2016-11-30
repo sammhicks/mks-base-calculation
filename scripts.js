f9d0e4aaf1ead16155f757c2ee18949b203aa3fc
Samuel Hicks <SamMHicks@hotmail.co.uk>
2016-11-30 16:22:18 +0000

    Created config loading function

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
-function load_version(http){
-	return http.get("https://raw.githubusercontent.com/BobPalmer/MKS/master/FOR_RELEASE/GameData/UmbraSpaceIndustries/MKS/MKS.version");
-}
-
 var app = angular.module('mks-calculation', []);
 app.controller('mks-calculation-controller', function($scope, $http) {
-	load_version($http).then((response) => {
+	function load_version() {
+		return $http.get("https://raw.githubusercontent.com/BobPalmer/MKS/master/FOR_RELEASE/GameData/UmbraSpaceIndustries/MKS/MKS.version");
+	}
+	
+	function load_config(config_location)
+	{
+		return new Promise(function(resolve, reject){
+			$http.get(config_location).catch(reject).then((response) => {
+				var config = {};
+				resolve(config);
+			});
+		});
+	}
+	
+	load_version().then((response) => {
 		$scope.version = response.data.VERSION;
 		$scope.ksp_version = response.data.KSP_VERSION;
+	}).then(() => {
+		load_config("https://raw.githubusercontent.com/BobPalmer/MKS/master/FOR_RELEASE/GameData/UmbraSpaceIndustries/MKS/Parts/Duna_Agriculture.cfg").then((response) => {
+			console.log(response.data);
+		});
 	});
 });
