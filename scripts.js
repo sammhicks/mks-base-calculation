4ac159cfcfc51f727f085036364fa122cab526c6
Samuel Hicks <SamMHicks@hotmail.co.uk>
2016-12-01 02:36:50 +0000

    Implemented crawling of MKS parts

--- scripts.js
+++ scripts.js
@@ -1,33 +1,78 @@
-var app = angular.module('mks-calculation', []);
-app.controller('mks-calculation-controller', function($scope, $http) {
-	function load_version() {
-		return $http.get("https://raw.githubusercontent.com/BobPalmer/MKS/master/FOR_RELEASE/GameData/UmbraSpaceIndustries/MKS/MKS.version");
-	}
+angular.module("mks-calculation", []).controller("mks-calculation-controller", ["$scope", "$http", function($scope, $http) {
+	var MKS_path = "FOR_RELEASE/GameData/UmbraSpaceIndustries/MKS/";
 	
-	function load_config(config_location)
+	var get_contents = (path) => $http.get("https://api.github.com/repos/bobpalmer/mks/contents/" + path);
+	
+	var get_file_data = (file_info) => new Promise(function(resolve, reject) {
+		if (file_info.type === "file")
+		{
+			$http.get(file_info.download_url).catch(reject).then((file_response) => {
+				resolve(file_response.data)
+			});
+		}
+		else
+		{
+			reject("Cannot download file \"" + file_info.download_url + "\"");
+		}
+	});
+	
+	var load_config_file = (file_info) => new Promise(function(resolve, reject) {
+		get_file_data(file_info).then((response) => {
+			var parse_result = config_parser.config.parse(response);
+			
+			if (parse_result.status)
+			{
+				resolve([parse_result.value]);
+			}
+			else
+			{
+				resolve([]);
+			}
+		}, reject);
+	});
+	
+	var load_config_dir = (dir_info) => new Promise(function(resolve, reject) {
+		get_contents(dir_info.path).then((response) => {
+			Promise.all(response.data.map(load_config)).then((responses) => {
+				resolve(responses.reduce((a, b) => a.concat(b)));
+			}, reject);
+		}, reject);
+	});
+	
+	function load_config(file)
 	{
-		return new Promise(function(resolve, reject){
-			$http.get(config_location).catch(reject).then((response) => {
-				var parse_result = config_parser.config.parse(response.data);
-				
-				if (parse_result.status)
+		switch (file.type)
+		{
+			case "file": {
+				if (file.name.endsWith(".cfg"))
 				{
-					resolve(parse_result.value);
+					return load_config_file(file);
 				}
 				else
 				{
-					reject(parse_result);
+					return Promise.resolve([]);
 				}
-			});
-		});
+			}
+			case "dir":
+				return load_config_dir(file);
+			default:
+				return Promise.resolve([]);
+		}
 	}
 	
-	load_version().then((response) => {
-		$scope.version = response.data.VERSION;
-		$scope.ksp_version = response.data.KSP_VERSION;
-	}).then(() => {
-		load_config("https://raw.githubusercontent.com/BobPalmer/MKS/master/FOR_RELEASE/GameData/UmbraSpaceIndustries/MKS/Parts/Duna_Agriculture.cfg").then((response) => {
-			console.log(response);
+	var load_configs = () => load_config_dir({path: MKS_path + "Parts"});
+	
+	get_contents(MKS_path + "MKS.version").then((response) => {
+		get_file_data(response.data).then((response) => {
+			$scope.version = response.VERSION;
+			$scope.ksp_version = response.KSP_VERSION;
+			$scope.$apply();
 		});
 	});
-});
+	
+	load_configs().then((parts) => {
+		console.log(parts);
+		$scope.parts = parts;
+		$scope.$apply();
+	});
+}]);
