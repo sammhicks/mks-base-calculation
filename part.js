f96fcd09c752f3669137ed8429a5ba469e328ab1
Samuel Hicks <SamMHicks@hotmail.co.uk>
2016-12-04 03:48:54 +0000

    Added more module information to parts
    parts now contains information regarding eTag, eMultiplier, the ModulePowerCoupler module and the ModulePowerDistributor module

--- part.js
+++ part.js
@@ -1,6 +1,49 @@
+function PowerCoupler(part) {
+	this.range = part.PowerCouplingRange;
+}
+
+function PowerDistributor(part) {
+	this.range = part.PowerDistributionRange;
+}
+
 function MKSPart(part) {
 	this.name = part.name;
 	this.title = part.title;
 	
+	this.eTag = part.eTag;
+	this.eMultiplier = part.eMultiplier;
 	
+	var powerCoupler = part.MODULE.filter((module) => module.name === "ModulePowerCoupler")[0];
+	if (powerCoupler !== undefined) {
+		this.powerCoupler = new PowerCoupler(powerCoupler);
+	}
+	
+	var powerDistributor = part.MODULE.filter((module) => module.name === "ModulePowerDistributor")[0];
+	if (powerDistributor !== undefined) {
+		this.powerDistributor = new PowerDistributor(powerDistributor);
+	}
+	
+	this.has_resources = part.RESOURCE != undefined;
+	this.resources = {};
+	
+	if (this.has_resources)
+	{
+		for (const resource of part.RESOURCE){
+			this.resources[resource.name] = resource;
+		}
+	}
+	
+	var is_converter = (module) => module.name === "ModuleResourceConverter_USI";
+	
+	this.has_converter = part.MODULE.some(is_converter);
+	this.converters = {};
+	
+	if (this.has_converter)
+	{
+		for (const module of part.MODULE) {
+			if (is_converter(module)) {
+				this.converters[module.ConverterName] = module;
+			}
+		}
+	}
 }
--- scripts.js
+++ scripts.js
@@ -33,9 +33,21 @@ angular.module("mks-calculation", ["ngStorage"]).controller("mks-calculation-con
 		return false;
 	}
 	
+	var has_module = (part, module_name) => part.MODULE !== undefined && part.MODULE.some((module) => module.name === module_name);
+	
+	var has_any_module = (part, module_names) => module_names.some((module_name) => has_module(part, module_name));
+	
+	function valid_part(part)
+	{
+		return has_any_module(part, [
+			"MKSModule",
+			"ModulePowerCoupler"
+		]);
+	}
+	
 	function map_parts()
 	{
-		$scope.parts = $localStorage.parts.map((part) => new MKSPart(part));
+		$scope.parts = $localStorage.parts.filter(valid_part).map((part) => new MKSPart(part));
 	}
 	
 	$http.get("https://raw.githubusercontent.com/BobPalmer/MKS/master/FOR_RELEASE/GameData/UmbraSpaceIndustries/MKS/MKS.version").then((response) => {
