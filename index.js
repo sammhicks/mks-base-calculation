5bbc581b89d9e8dfec72879958c3ed29177ce003
Samuel Hicks <SamMHicks@hotmail.co.uk>
2016-12-05 16:10:18 +0000

    You can now drag parts onto the base parts

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 "use strict";
 
-angular.module("mks-calculation", ["ngStorage"]).controller("mks-calculation-controller", function($scope, $http, $localStorage, $sessionStorage) {
+angular.module("mks-calculation", ["ngStorage", "dndLists"]).controller("mks-calculation-controller", function($scope, $http, $localStorage, $sessionStorage) {
 	loadData($scope, $http, $localStorage);
+	
+	$scope.baseParts = [];
+	
+	$scope.dropPart = function(index, item) {
+		console.log("Dropped", index, item);
+		
+		$scope.baseParts.splice(index, 0, item);
+		
+		return true;
+	}
 });
