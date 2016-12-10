72a819edc803c77a3cd9d6afc7c8ee4862bbd588
Samuel Hicks <SamMHicks@hotmail.co.uk>
2016-12-10 18:35:35 +0000

    Created service dependencies for the index angular app

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-angular.module("mksCalculation", ["ngStorage", "dndLists"]).controller("mksCalculationController", function($scope, $http, $localStorage, $sessionStorage) {
+angular.module("mksCalculation", ["ngStorage", "dndLists"]).controller("mksCalculationController", ["$scope", "$http", "$localStorage", "$sessionStorage", function($scope, $http, $localStorage, $sessionStorage) {
 	loadData($scope, $http, $localStorage).then(function() {
 		$scope.baseParts = [];
 	}, console.error);
-});
+}]);
